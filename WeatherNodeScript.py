95ab27ad811c243bfe7c417b08c9aa3c255c34c3
necropaz <jan-huber@hispeed.ch>
2016-07-15 11:43:32 +0200

    v0.1

--- WeatherNodeScript.py
+++ WeatherNodeScript.py
@@ -2,7 +2,7 @@ import sys
 import json
 import time
 import os
-import IOTAWeatherNode as Node
+import iota
 
 #Here you can enter the simulatet weather Data
 temperature="30"	#Temperature in Celsius (only for simulation)
@@ -16,37 +16,30 @@ address=input("Address IOTA Node? ")
 
 #Enter here your seed.
 seed=input("Address IOTA Node Seed? ")
-
-
-promotionText="This is my promotion!"
-commandWeather="{'command':'getWeather'}"
-commandPromotion="{'command':'sendPromotion','promotion':'"+promotionText+"'}"
-
-transactionOld=Node.IOTA.searchNewTransaction2(seed)
+node=iota(seed)
 print("Weather Node is Running.")
 print("Waiting until the Weatehr Node recived a request.")
+txCounter=node.txCounter
 while True:
 	time.sleep(5)
-	self.transaction=Node.IOTA.searchNewTransaction2(self.seed)
-	if self.transactionOld!=self.transaction:
-		self.transactionOld=self.transaction
+	self.transaction=node.searchNewTransaction()
+	if txCounter!=node.txCounter
 		break            
-            
-	commandRecived=Node.IOTA.searchMessage(transaction)
-	commandRecived=commandRecived.replace('\'','\"')
-	jsonData=json.loads(commandRecived)
-	if jsonData['command']=="getWeather" :
-		#message=Node.Weather.readWeather()
-		message="{'Temperature':'"+temperature+"', 'Humidity':'"+humidity+"','Pressure':'"+pressure+"'}"
-		print("Node.IOTA.sendMessage(self,"+ seed +","+ address+","+ message+", '1'")
-		Node.IOTA.sendMessage(seed, address, message, '1')
-		print("Succesfully send request.")
-	elif jsonData['command']=="sendPromotion":
-		print("Promotion request recived")
-		print("Promotion Text:"+jsonData['promotion'])
-		#Node.Weather.scrollText(jsonData['promotion'])
 
-	else:
-		print("Can't read sucessfully a command."+jsonData)
-	print("Temperatur: "+jsonData['temperature']+"Humidity: "+jsonData['humidity']+"Pressure: "+jsonData['pressure'])
+commandRecived=Node.IOTA.searchMessage(transaction)
+commandRecived=commandRecived.replace('\'','\"')
+jsonData=json.loads(commandRecived)
+if jsonData['command']=="getWeather" :
+	#message=Node.Weather.readWeather()
+	message="{'Temperature':'"+temperature+"', 'Humidity':'"+humidity+"','Pressure':'"+pressure+"'}"
+	print(message)
+	node.sendMessage(address, message, '1')
+	print("Succesfully send request.")
+elif jsonData['command']=="sendPromotion":
+	print("Promotion request recived")
+	print("Promotion Text:"+jsonData['promotion'])
+	#Node.Weather.scrollText(jsonData['promotion'])
+else:
+	print("Can't read sucessfully a command."+jsonData)
 
+print("End Iota Weather Node!")
